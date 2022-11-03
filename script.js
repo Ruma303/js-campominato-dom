128debe1f14a32bcc833f0ec2f74528e258d88f7
Ruma <matteoh8@hotmail.it>
2022-11-03 17:41:10 +0100

    non funzionante

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const eleCell = document.querySelector('.cell');
 const title = document.querySelector('#title');
 const select = document.querySelector('#select');
 const btn = document.querySelector('#play-btn');
+const arrNumFound = []; //array numeri trovati, fuori dal ciclo o viene resettato sempre.
 
 btn.addEventListener('click', function() {
     eleGrid.classList.remove('hidden');
@@ -13,18 +14,28 @@ btn.addEventListener('click', function() {
     const sideSquare = Math.sqrt(numCells);
     eleGrid.style.setProperty('--sideSquare', sideSquare); 
     
-    const arrNumFound = []; //array numeri trovati, fuori dal ciclo o viene resettato sempre.
     for (let i = 1; i <= numCells; i++) { 
         
         let randomNumber;
-        do {   //verifica numero 
-            randomNumber = getRandomNum(1, numCells); //genera numero da 1 a select.value
-            //ciclo for per creare 16 numeri random
-            
-            //se è incluso rieseguo il blocco, altrimenti vado sul while;
-        } while (arrNumFound.includes(randomNumber)) //finché array == numero generato
-        arrNumFound.push(randomNumber); //pusha il numero nell'array
+        for (let e = 0; e<16; e++) {//ciclo percreare 16 numeri da 1 a max
+            do {   //verifica numero 
+                randomNumber = getRandomNum(1, numCells); //genera numero da 1 a select.value
+                //se è incluso rieseguo il blocco, altrimenti vado sul while;
+            } while (arrNumFound.includes(randomNumber)) //finché array == numero generato
+            arrNumFound.push(randomNumber); //pusha il numero nell'array
+            console.log(arrNumFound);
 
+            if (arrNrandom.includes(randomNumber)) {
+                eleCell.style.color = "red";
+            }
+
+            /*if (!arrNumFound.includes(randomNumber)) {
+                eleCell.classList.add('boom');
+            } else {
+                eleCell.classList.add('ok');
+            }*/
+        }
+        console.log(arrNumFound);
         //creazione div e classe delle celle
         const eleCell = document.createElement('div');
         eleCell.classList.add('cell');
@@ -37,18 +48,16 @@ btn.addEventListener('click', function() {
 
         //funzione toogle per colorare i quadrati sul click
         eleCell.addEventListener('click', function(){
-            if (arrNumFound.includes(randomNumber)) {
+            /*if (arrNumFound.includes(randomNumber)) {
                 eleCell.classList.remove('ok');
                 eleCell.classList.add('boom');
             } else {
                 eleCell.classList.remove('boom');
                 eleCell.classList.add('ok');
-            }
+            }*/
             //output numero cliccato in console
             console.log(cellNumber)
         })}
-
-        console.log(arrNumFound);
     //funzione creazione numeri random
     function getRandomNum(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
