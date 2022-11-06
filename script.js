c125fd7579237c09c7b32720acd7e9015d184ca5
Ruma <matteoh8@hotmail.it>
2022-11-06 15:16:55 +0100

    codice aggiustato

--- script.js
+++ script.js
@@ -2,55 +2,124 @@ const eleGrid = document.querySelector('.grid');
 const eleCell = document.querySelector('.cell');
 const title = document.querySelector('#title');
 const select = document.querySelector('#select');
-const btn = document.querySelector('#play-btn');
-const arrNumFound = []; //array numeri trovati, fuori dal ciclo o viene resettato sempre.
+const btnPlay = document.querySelector('#play-btn');
+const btnHelp = document.querySelector('#help-btn');
+const win = document.querySelector('#win');
+const lost = document.querySelector('#lost');
+let arrMines = [];
+let score;
+let maxScore;
 
-const numCells = parseInt(select.value);
-const sideSquare = Math.sqrt(numCells);
 
-//ciclo per creare un'array di 16 numeri da 1 a max
-let randomNumber;
-for (let e = 1; e<=16; e++) {
-    do {  //verifica numero 
-        randomNumber = getRandomNum(1, numCells); //genera numero da 1 a select.value
-        //se è incluso rieseguo il blocco, altrimenti vado sul while;
-    } while (arrNumFound.includes(randomNumber)) //finché array == numero generato
-    arrNumFound.push(randomNumber); //pusha il numero nell'array
-}
-console.log(arrNumFound);
+//Al click del bottone Play
+btnPlay.addEventListener('click', function() {
+    const numCells = parseInt(select.value);
+    const nMines = 16;
+    score = 0;
+    maxScore = numCells - nMines;
+	arrMines = generateMines(nMines, 1, numCells);
+	console.log(arrMines.sort((a, b) => a - b)); //print ordine crescente
 
-//funzione creazione numeri random
-function getRandomNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-btn.addEventListener('click', function() {
     eleGrid.classList.remove('hidden');
     title.classList.add('hidden');
-    eleGrid.innerHTML = '';
-
+    eleGrid.innerHTML = ''; //reset
+    //eleGrid.replaceChildren(); //altro metodo
+    
+    //creo le celle dinamicamente
+    const sideSquare = Math.sqrt(numCells); 
     eleGrid.style.setProperty('--sideSquare', sideSquare); 
     
     for (let i = 1; i <= numCells; i++) { 
-
         //creazione div e classe delle celle
         const eleCell = document.createElement('div');
         eleCell.classList.add('cell');
-        eleGrid.append(eleCell);
-        
-        //creazione numeri nelle celle
-        let cellNumber = i;
         eleCell.classList.add('number');
-        eleCell.innerHTML += cellNumber;
-        
-        //evento per colorare i quadrati sul click
-        eleCell.addEventListener('click', function(){
-            console.log(cellNumber)
-            if (arrNumFound.includes(randomNumber)) {
-                //eleCell.classList.add('boom');
-            } else {
-                eleCell.classList.remove('boom');
-                eleCell.classList.add('ok');
-            }
-        })}
+        eleCell.innerHTML = i; //scrive i numeri dentro le celle
+        eleGrid.append(eleCell);
 
-})
+        //Colora le celle al click
+        eleCell.addEventListener('click', toggleCell) 
+        }
+    }
+)
+
+//Bottone Help
+btnHelp.addEventListener('click', ()=> {
+    //se c'é scritto Show Help torna alla schermata principale
+    if(btnHelp.dataset.state == 'show-help') {
+        btnHelp.dataset.state = 'show-game';
+        btnHelp.innerHTML = 'Back to game';
+        eleGrid.classList.add('hidden');
+        title.classList.remove('hidden');
+    } //altrimenti torna al gioco 
+    else if (btnHelp.dataset.state == 'show-game') { 
+        btnHelp.dataset.state = 'show-help';
+        btnHelp.innerHTML = 'Show Help';
+        eleGrid.classList.remove('hidden');
+        title.classList.add('hidden');
+        }
+    }
+)
+
+//funzione click sulla cella
+/*function toggleCell () {
+    this.classList.toggle('active');   
+}*/
+function toggleCell() {
+	// funzione per determinare l'elemento a cui determinare il tipo di cella
+	const cellNumber = parseInt(this.innerHTML);
+
+	if (arrMines.includes(cellNumber)) { // arrMines.includes(i) questa funzione grazie al concetto di "closure" ma se la funzione e' stata definita in uno scope che include la i
+		this.classList.add('bomb');
+		disableAllCells(true);
+        lost.classList.remove('hidden');
+        lost.innerHTML = `Hai perso! Il tuo punteggio é ${score}`
+		// alert('Hai perso! Il tuo punteggio é: ' + score);
+	} else {
+		this.removeEventListener('click', toggleCell); // evitiamo di accumulare punteggio cliccando su celle giá aperte
+		score++; // incremento score al ogni click di cella senza bomba
+		this.classList.add('ok');
+		if (score == maxScore) {
+			disableAllCells(false);
+			// alert('Complimenti hai vinto! Il tuo punteggio é: ' + score);
+            win.classList.remove('hidden');
+            win.innerHTML = `Hai vinto! Il tuo punteggio é ${score}`
+		}
+	}
+}
+
+//Funzione per generare le mine
+function generateMines(nMines, min, max) {
+	const arrRandoms = [];
+    //forse serve dichiarare 
+    let randomNumber;
+	for (let i = 0; i < nMines; i++) {
+		do {
+			randomNumber = getRandomInteger(min, max);
+		} while (arrRandoms.includes(randomNumber))
+		arrRandoms.push(randomNumber);
+	    }
+	return arrRandoms;
+}
+
+//funzione genera numero random
+function getRandomInteger(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+
+function disableAllCells(showMines) {
+	const listCells = eleGrid.querySelectorAll('.cell');
+	// console.log(listCells);
+	for (let i = 0; i < listCells.length; i++) {
+		// se é una bomba la illumina
+		const cellNumber = parseInt(listCells[i].innerHTML);
+		// console.log(cellNumber);
+		if (showMines && arrMines.includes(cellNumber)) {
+			listCells[i].classList.add('mine');
+		}
+		// toglie l'event listener dalla cella per disattivarla
+		listCells[i].removeEventListener('click', toggleCell);
+		// console.log('listener rimosso');
+	}
+}
